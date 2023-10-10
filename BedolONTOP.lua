00ca0edd9f2015e1efd687380a9cbb85673890fa
Mc4121ban <105984396+Mc4121ban@users.noreply.github.com>
2023-10-10 16:09:51 +0700

    Update BedolONTOP.lua

--- BedolONTOP.lua
+++ BedolONTOP.lua
@@ -1,1 +1,1 @@
-switch : loadstring(game:HttpGet(('https://raw.githubusercontent.com/Mc4121ban/RobloxScirpt/main/loader.lua')))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Mc4121ban/RobloxScirpt/main/loader.lua')))()
